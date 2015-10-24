c4a5bde756ab945b588f825fd6704a5666b5c54a
Louis Pilfold <louis@lpil.uk>
2015-10-24 20:32:54 +0100

    Thumbs up, let's do this.

---
+++ README.md
@@ -0,0 +1,12 @@
+My 'puter.
+==========
+
+Setting up a computer is annoying, so let's automate it with Ansible.
+
+## Usage
+
+```
+./run.sh
+```
+
+Tada!
---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e # Exit if any fail
+set -u # Error on unset vars
+set -o pipefail
+
+export ANSIBLE_NOCOWS=1
+
+command -v ansible-playbook >/dev/null 2>&1 || {
+  sudo apt-get install ansible
+}
+
+ansible-playbook playbook.yml -i hosts.ini --ask-sudo-pass
